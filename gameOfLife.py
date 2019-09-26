80a35bcac14a92dd47d1260e1793fa3f062a02ea
Jung Hyun Chun <ouoabcde@naver.com>
2019-09-26 23:17:27 +0900

    read data from file made into makeBoardFromFile function

--- gameOfLife.py
+++ gameOfLife.py
@@ -32,46 +32,50 @@ class GameOfLifeEngine:
         self.nSurvive = nSurvive
         return cell
 
+def makeBoardFromFile(initial_data):
+    with open(initial_data, 'r') as initial_data:
+        data = initial_data.readline()
+        board_size = data.split()
+        NUM_ROWS = int(board_size[0])
+        NUM_COLS = int(board_size[1])
+        board = np.zeros(NUM_ROWS * NUM_COLS).reshape(NUM_ROWS, NUM_COLS)
+        print("NUM_ROWS: {}".format(NUM_ROWS))
+        print("NUM_COLS: {}".format(NUM_COLS))
+        count = 1
+        while data:
+            print("Line {}: {}".format(count, data))
+            data = initial_data.readline()
+            count += 1
+            if count == 2:
+                NUM_CELLS_INIT = int(data)
+                print("NUM_CELLS_INIT: {}".format(NUM_CELLS_INIT))
+            elif 2 < count <= NUM_CELLS_INIT + 2: # now it's spots
+                location = data.split()
+                row = int(location[0])
+                col = int(location[1])
+                board[row, col] = ALIVE
+        return board
+
 def randomBoard(height, width): 
     """returns a board of width x height random values"""
     return np.random.choice(vals, width * height, p=[0.2, 0.8]).reshape(height, width)
 
 def main():
     if len(sys.argv) == 2:
-        with open(sys.argv[1], 'r') as initial_data:
-            data = initial_data.readline()
-            board_size = data.split()
-            NUM_ROWS = int(board_size[0])
-            NUM_COLS = int(board_size[1])
-            board = np.zeros(NUM_ROWS * NUM_COLS).reshape(NUM_ROWS, NUM_COLS)
-            print("NUM_ROWS: {}".format(NUM_ROWS))
-            print("NUM_COLS: {}".format(NUM_COLS))
-            count = 1
-            while data:
-                print("Line {}: {}".format(count, data))
-                data = initial_data.readline()
-                count += 1
-                if count == 2:
-                    NUM_CELLS_INIT = int(data)
-                    print("NUM_CELLS_INIT: {}".format(NUM_CELLS_INIT))
-                elif 2 < count <= NUM_CELLS_INIT + 2: # now it's spots
-                    location = data.split()
-                    row = int(location[0])
-                    col = int(location[1])
-                    board[row, col] = ALIVE
-            plt.title("Jung Hyun's Game of Life")
-            engine = GameOfLifeEngine(board)
-            generation = 0
-            while True:
-                if generation == 0:
-                    plt.ion()
-                generation += 1
-                plt.imshow(board, cmap='binary')
-                plt.show()
-                engine.applyGameRules()
-                # set up animation
-                print('Generation: {} Birth: {} Survive: {}'.format(generation, engine.nBirth, engine.nSurvive))
-                plt.pause(0.4)
+        board = makeBoardFromFile(sys.argv[1])
+        plt.title("Jung Hyun's Game of Life")
+        engine = GameOfLifeEngine(board)
+        generation = 0
+        while True:
+            if generation == 0:
+                plt.ion()
+            generation += 1
+            plt.imshow(board, cmap='binary')
+            plt.show()
+            engine.applyGameRules()
+            # set up animation
+            print('Generation: {} Birth: {} Survive: {}'.format(generation, engine.nBirth, engine.nSurvive))
+            plt.pause(0.4)
     elif len(sys.argv) == 1:
         random_width = random.randint(0, 200)
         random_height = random.randint(0, 200)
